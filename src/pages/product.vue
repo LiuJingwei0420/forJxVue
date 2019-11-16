dcd103038a16017f3fe424a53a71d4943226f215
JackBean <chenjianbing@songguo7.com>
2019-11-16 13:16:17 +0800

    动画一

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -39,11 +39,11 @@
       <div class="item-video">
         <h2>60帧超慢动作摄影<br/>慢慢回味每一瞬间的精彩</h2>
         <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
-        <div class="video-bg"></div>
+        <div class="video-bg" @click="showSlide=true"></div>
         <div class="video-box">
-          <div class="overlay"></div>
-          <div class="video">
-            <span class="icon-close"></span>
+          <div class="overlay" v-if="showSlide"></div>
+          <div class="video" v-bind:class="{'slide':showSlide}">
+            <span class="icon-close" @click="showSlide=false"></span>
             <video src="/imgs/product/video.mp4" muted autoplay controls="controls"></video>
           </div>
         </div>
@@ -63,6 +63,7 @@
     },
     data(){
       return {
+        showSlide:false,
         swiperOption:{
           autoplay:true,
           slidesPerView:3,
@@ -158,7 +159,6 @@
           cursor:pointer;
         }
         .video-box{
-          
           .overlay{
             @include position(fixed);
             background-color:#333333;
@@ -167,12 +167,18 @@
           }
           .video{
             position:fixed;
-            top:50%;
+            top:-50%;
             left:50%;
             transform:translate(-50%,-50%);
             z-index:10;
             width:1000px;
             height:536px;
+            opacity:0;
+            transition:all .6s;
+            &.slide{
+              top:50%;
+              opacity:1;
+            }
             .icon-close{
               position:absolute;
               top:20px;
