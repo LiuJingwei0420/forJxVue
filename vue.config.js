b535a2d33f7e102b4ae6aea949566750e30d6e20
JackBean <chenjianbing@songguo7.com>
2019-12-19 21:46:50 +0800

    删除预加载

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,8 @@ module.exports = {
   // outputDir:'dist',
   // indexPath:'index2.html',
   // lintOnSave:false,
-  productionSourceMap:false
+  productionSourceMap:true,
+  chainWebpack:(config)=>{
+    config.plugins.delete('prefetch');
+  }
 }
